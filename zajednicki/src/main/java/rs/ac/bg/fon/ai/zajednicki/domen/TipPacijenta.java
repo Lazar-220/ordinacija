0e9bf3c0cc83ff95cc0bf4d428b08980565eeed2
LazarMilosavljevic <milosavljeviclazar56@gmail.com>
2025-08-06 14:08:34 +0200

    Završeno dokumentovanje klase TipPacijenta

--- zajednicki/src/main/java/rs/ac/bg/fon/ai/zajednicki/domen/TipPacijenta.java
+++ zajednicki/src/main/java/rs/ac/bg/fon/ai/zajednicki/domen/TipPacijenta.java
@@ -13,18 +13,45 @@ import rs.ac.bg.fon.ai.zajednicki.domen.Enum.Pol;
 import rs.ac.bg.fon.ai.zajednicki.domen.Enum.StarosnaDob;
 
 /**
- *
- * @author milos
+ * Predstavlja tip pacijenta koji je definisan starosnom dobi i polom.
+ * 
+ * Ova klasa implementira interfejs ApstraktniDomenskiObjekat, što omogućava komunikaciju sa bazom podataka.
+ * 
+ * @author Lazar Milosavljević
  */
 public class TipPacijenta implements ApstraktniDomenskiObjekat {
 
+    /**
+     * Jedinstveni identifikator tipa pacijenta.
+     */
     private int idTipPacijenta;
+
+    /**
+     * Starosna dob pacijenta.
+     */
     private StarosnaDob starosnaDob;
+
+    /**
+     * Pol pacijenta.
+     */
     private Pol pol;
 
+    /**
+     * Podrazumevani konstruktor. Inicijalizuje tip pacijenta sa podrazumevanim vrednostima.
+     */
     public TipPacijenta() {
     }
 
+    /**
+     * Parametrizovani konstruktor. Inicijalizuje tip pacijenta sa zadatim vrednostima.
+     * 
+     * @param idTipPacijenta Jedinstveni identifikator tipa pacijenta. Ne sme biti manji od 0.
+     * @param starosnaDob Starosna dob pacijenta. Ne sme biti null.
+     * @param pol Pol pacijenta. Ne sme biti null.
+     * 
+     * @throws java.lang.IllegalArgumentException ako je id manji od 0.
+     * @throws java.lang.NullPointerException ako je starosna dob ili pol null.
+     */
     public TipPacijenta(int idTipPacijenta, StarosnaDob starosnaDob, Pol pol) {
 //        this.idTipPacijenta = idTipPacijenta;
 //        this.starosnaDob = starosnaDob;
@@ -32,47 +59,98 @@ public class TipPacijenta implements ApstraktniDomenskiObjekat {
     	setIdTipPacijenta(idTipPacijenta);
     	setPol(pol);
     	setStarosnaDob(starosnaDob);
-    	
     }
 
+    /**
+     * Vraća ID tipa pacijenta.
+     * 
+     * @return ID tipa pacijenta kao int.
+     */
     public int getIdTipPacijenta() {
         return idTipPacijenta;
     }
 
+    /**
+     * Postavlja ID tipa pacijenta.
+     * 
+     * @param idTipPacijenta ID tipa pacijenta. Ne sme biti manji od 0.
+     * 
+     * @throws java.lang.IllegalArgumentException ako je id manji od 0.
+     */
     public void setIdTipPacijenta(int idTipPacijenta) {
-    	if(idTipPacijenta<0) {
+    	if(idTipPacijenta < 0) {
     		throw new IllegalArgumentException("id tipa pacijenta ne sme biti manji od 0");
     	}
         this.idTipPacijenta = idTipPacijenta;
     }
 
+    /**
+     * Vraća starosnu dob pacijenta.
+     * 
+     * @return Starosna dob kao StarosnaDob.
+     */
     public StarosnaDob getStarosnaDob() {
         return starosnaDob;
     }
 
+    /**
+     * Postavlja starosnu dob pacijenta.
+     * 
+     * @param starosnaDob Starosna dob. Ne sme biti null.
+     * 
+     * @throws java.lang.NullPointerException ako je starosna dob null.
+     */
     public void setStarosnaDob(StarosnaDob starosnaDob) {
-    	if(starosnaDob==null) {
+    	if(starosnaDob == null) {
     		throw new NullPointerException("starosna dob pacijenta ne sme biti null");
     	}
         this.starosnaDob = starosnaDob;
     }
 
+    /**
+     * Vraća pol pacijenta.
+     * 
+     * @return Pol kao Pol.
+     */
     public Pol getPol() {
         return pol;
     }
 
+    /**
+     * Postavlja pol pacijenta.
+     * 
+     * @param pol Pol pacijenta. Ne sme biti null.
+     * 
+     * @throws java.lang.NullPointerException ako je pol null.
+     */
     public void setPol(Pol pol) {
-    	if(pol==null) {
+    	if(pol == null) {
     		throw new NullPointerException("pol pacijenta ne sme biti null");
     	}
         this.pol = pol;
     }
 
+    /**
+     * Računa hashCode na osnovu starosne dobi i pola.
+     * 
+     * @return Hash code vrednost objekta.
+     */
     @Override
     public int hashCode() {
     	return Objects.hash(starosnaDob, pol);
     }
 
+    /**
+     * Poredi dva objekta TipPacijenta na osnovu starosne dobi i pola.
+     * 
+     * @param obj Objekat za poređenje.
+     * 
+     * @return 
+     * <ul>
+     * <li><b>true</b> - ako objekti imaju istu starosnu dob i pol.</li>
+     * <li><b>false</b> - u suprotnom.</li>
+     * </ul>
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -91,10 +169,11 @@ public class TipPacijenta implements ApstraktniDomenskiObjekat {
         return this.pol == other.pol;
     }
 
-    
-
-    
-
+    /**
+     * Vraća String reprezentaciju objekta TipPacijenta.
+     * 
+     * @return Reprezentacija u formatu: "TipPacijenta{starosnaDob=####, pol=####}"
+     */
     @Override
     public String toString() {
         return "TipPacijenta{" + "starosnaDob=" + starosnaDob + ", pol=" + pol + '}';
